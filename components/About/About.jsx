3de71576d88b31472b2bc49000faf600070f9676
vgalante2 <vgalante0@gmail.com>
2023-12-01 18:41:40 -0500

    about section

---
+++ components/About/About.jsx
@@ -0,0 +1,15 @@
+import styles from './About.module.scss';
+
+
+function About() {
+
+    return (
+        <div className={styles.aboutSection}> 
+        <div className={styles.aboutContainer}>
+         <h1 className={styles.heading}>About Us</h1>
+        </div>
+        </div>
+    );
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header/Header.jsx';
 import BigText from '../components/BigText/BigText.jsx';
 import ClothingSection from '../components/ClothingSection/ClothingSection.jsx';
 import Promo from '../components/Promo/Promo.jsx';
+import About from '../components/About/About.jsx';
 import Footer from '../components/Footer/Footer.jsx';
 
 export default function HomePage() {
@@ -12,6 +13,7 @@ export default function HomePage() {
       <ClothingSection />
       <BigText />
       <Promo />
+      <About />
       <Footer />
       {/* Other components specific to the homepage */}
     </>
