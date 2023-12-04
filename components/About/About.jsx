48910da822ac7d5bf8e09c38c29fb64556b3ef2b
vgalante2 <vgalante0@gmail.com>
2023-12-03 23:54:30 -0500

    updates

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
     return (
         <div className={styles.aboutSection}> 
         <div className={styles.aboutContainer}>
-         <h1 className={styles.heading}>About Us Section</h1>
+         <h1 className={styles.heading}>About Us Sec</h1>
         </div>
         </div>
     );
