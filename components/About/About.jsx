219889a9442b19d1aa761a6694ea4d2b55a5916e
vgalante2 <vgalante0@gmail.com>
2023-12-21 20:55:51 -0500

    about switch

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -5,11 +5,13 @@ function About() {
 
     return (
         <div className={styles.aboutSection}> 
+        <img className={styles.heartOne} src="/heart1.png" alt="star" />
         <div className={styles.aboutContainerTwo}>
         <h1 className={styles.heading}>
         better keep an eye out...its limited</h1>
         <p  className={styles.paragraph}>new and exclusive drops ever month</p>
         </div>
+        <img className={styles.heartTwo} src="/heart2.png" alt="star2" />
         </div>
     );
 }
