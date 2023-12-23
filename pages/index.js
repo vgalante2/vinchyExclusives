30d7548a8e3b35ed3180e20419ce5979a6f642ef
vgalante2 <vgalante0@gmail.com>
2023-12-23 12:19:39 -0500

    welcome page addition

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-
+import { useState } from 'react';
+import styles from "/styles/Welcome.module.scss";
 import Header from '../components/Header/Header.jsx';
 // import BigText from '../components/BigText/BigText.jsx';
 import ClothingSection from '../components/ClothingSection/ClothingSection.jsx';
@@ -8,6 +9,21 @@ import BestSellers from '../components/ClothingSection/BestSellers.jsx';
 import Footer from '../components/Footer/Footer.jsx';
 
 export default function HomePage() {
+const [entered, setEntered] = useState(false);
+
+if (!entered) {
+return (
+<div className={styles.WelcomeContainer}> 
+<video className={styles.WelcomeVideo} src="/VINCHY_PROMO.mp4" autoPlay loop muted />
+<div className={styles.contentContainer}>
+<h1 className={styles.h1}>vinchy exclusives <span className={styles.span}>*</span></h1>
+<button className={styles.WelcomeBtn} onClick={() => setEntered(true)} > Enter </button>
+</div>
+</div>
+)
+}
+
+
   return (
     <>
       <Header />
