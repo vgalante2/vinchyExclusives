027a9220061622d83882a94f327871eb35fbd325
vgalante2 <vgalante0@gmail.com>
2023-12-30 17:10:01 -0500

    product page update

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -11,10 +11,31 @@ function ProductId({ productData }) {
   // Render the product details
   return (
     <div className={styles.productSection}>
+     <div className={styles.ImageContainer}>
+     <img className={styles.ProductImage} src={productData.imgSrc} alt={productData.name} />
+     </div>
+     <div className={styles.InfoContainer}>
+     <div className={styles.NameContainer}>
       <h1 className={styles.name}>{productData.name}</h1>
-      <img className={styles.image} src={productData.imgSrc} alt={productData.name} />
+      <p className={styles.price}>${productData.price}</p>
       <p className={styles.description}>{productData.description}</p>
-      <p className={styles.price}>Price: ${productData.price}</p>
+      </div>
+      <div className={styles.sizeContainer}>
+        <p className={styles.sizeHead}>size</p>
+       <div className={styles.sizes}>
+       <button className={styles.small}>{productData.size[0]}</button>
+       <button className={styles.medium}>{productData.size[1]}</button>
+       <button className={styles.large}>{productData.size[2]}</button>
+       </div>
+      </div>
+      <div className={styles.CheckoutContainer}>
+     {/* <button></button> */}
+     <div className={styles.CartContainer}>
+     <button className={styles.ATCContainer}>Add to Cart</button>
+     <button className={styles.CheckContainer}>Checkout</button>
+     </div>
+      </div>
+      </div>
     </div>
   );
 }
