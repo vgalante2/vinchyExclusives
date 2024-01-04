7f23bd2ea63483e7d15cea87ad530b2c54fa24dd
vgalante2 <vgalante0@gmail.com>
2024-01-04 14:18:18 -0500

    cart info debug

--- components/Navbar/Cart/Cart.jsx
+++ components/Navbar/Cart/Cart.jsx
@@ -1,27 +1,43 @@
 import React, { useState, useEffect } from 'react';
  import styles from "./Cart.module.scss"
-
+ import ClearIcon from '@mui/icons-material/Clear';
 
 
 function Cart() {
     const [cartItems, setCartItems] = useState([]);
+
 
     useEffect(() => {
       const storedCartItems = JSON.parse(localStorage.getItem('cart')) || [];
       setCartItems(storedCartItems);
     }, []);
 
+
+    const handleDeletedItem = (id) => {
+        const updatedCartItems = cartItems.filter(item => item.id !== id);
+        setCartItems(updatedCartItems);
+        localStorage.setItem('cart', JSON.stringify(updatedCartItems));
+      };
+
+    
+
 return (
 <div className={styles.cartSection}>
 <h1>Your Cart</h1>
 <div className={styles.cartContent}>
 {cartItems.map((item, index) => (
-        <div key={index}>
-        <p>{item.name} - Size: {item.selectedSize} - Quantity: {item.quantity}</p>
-          <p> ${item.price}</p>
+        <div key={index} className={styles.CartSetup}>
+        <img className={styles.cartImg} src={item.imgSrc} alt="clothing_pic" />
+        <p>{item.name} </p> 
+        <p> ${item.price}</p>
+        <p>- Size: {item.selectedSize} </p> 
+        <p>Quantity: {item.quantity}</p>
+          
+          <ClearIcon onClick={() => handleDeletedItem(item.id)} className={styles.delete} />
         </div>
       ))}
 </div>
+     <h1>Total: $  </h1>
 </div>
 );
  }
